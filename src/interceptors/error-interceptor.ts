3016a0d8c9d767e078e9a9d1e7404666e3dbd218
Jeremias Miguel <jjjeremiasmiguel@gmail.com>
2020-05-19 21:24:20 -0300

    Tratamento padrão para outros erros com AlertController

--- src/interceptors/error-interceptor.ts
+++ src/interceptors/error-interceptor.ts
@@ -3,11 +3,12 @@ import { HttpInterceptor, HttpRequest, HttpHandler, HttpEvent, HTTP_INTERCEPTORS
 import { Observable } from 'rxjs';
 import { catchError } from 'rxjs/operators';
 import { StorageService } from 'src/services/storage.service';
+import { AlertController } from '@ionic/angular';
 
 @Injectable()
 export class ErrorInterceptor implements HttpInterceptor {
 
-    constructor(public storageService: StorageService) {
+    constructor(public storageService: StorageService, public alertController: AlertController) {
 
     }
 
@@ -28,17 +29,50 @@ export class ErrorInterceptor implements HttpInterceptor {
                         console.log(errorObj);
 
                         switch(errorObj.status) {
+                            case 401:
+                                this.handle401();
+                                break;
                             case 403:
                                 this.handle403();
                                 break;
+                            default:
+                                this.handleDefaultError(errorObj);
                         }
                        
                         return Observable.throw(errorObj);
                     })) as any;
     }
 
+    async handle401() {
+        const alert = await this.alertController.create({
+            header: 'Erro 401: falha de autenticação',
+            message: 'Email e/ou senha incorretos',
+            buttons: [
+                {
+                    text: 'Ok'
+                }
+            ]
+        });
+
+        await alert.present();
+    }
+
     handle403() {
         this.storageService.setLocalUser(null);
+    }
+
+    async handleDefaultError(errorObj) {
+        const alert = await this.alertController.create({
+            header: 'Erro ' + errorObj.status + ': ' + errorObj.error,
+            message: errorObj.message,
+            buttons: [
+                {
+                    text: 'Ok'
+                }
+            ]
+        });
+
+        await alert.present();
     }
 }
 
